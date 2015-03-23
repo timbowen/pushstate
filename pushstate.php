6788d1416e78bf3d9a326e3a0343bbaa5a6d2f9c
Tim Bowen <tim@creativeslice.com>
2015-03-23 14:50:06 -0700

    renaming so it doesn't auto-update

--- pushstate.php
+++ pushstate.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * Plugin Name: Pushstate
- * Version: 1.0
+ * Plugin Name: AJAX Pushstate
+ * Version: 0.9
  * Plugin URI: http://www.joinerylabs.com/
  * Description: Asynchronous loading for Wordpress.
  * Author: Trip Grass
@@ -9,12 +9,12 @@
  * Requires at least: 4.0
  * Tested up to: 4.0
  *
- * Text Domain: pushstate
+ * Text Domain: ajax-pushstate
  * Domain Path: /lang/
  *
  * @package WordPress
  * @author Trip Grass
- * @since 1.0.0
+ * @since 0.9.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
@@ -30,17 +30,17 @@ if(is_admin()){
 	require_once( 'includes/lib/class-pushstate-admin.php' );
 }
 else{
-	require_once( 'includes/lib/class-pushstate-front.php' );	
+	require_once( 'includes/lib/class-pushstate-front.php' );
 }
 
 /**
  * Returns the main instance of pushstate to prevent the need to use globals.
  *
- * @since  1.0.0
+ * @since  0.9.0
  * @return object pushstate
  */
 function pushstate () {
-	$instance = pushstate::instance( __FILE__, '1.0.0' );
+	$instance = pushstate::instance( __FILE__, '0.9.0' );
 
 	if ( is_null( $instance->settings ) ) {
 		$instance->settings = pushstate_Settings::instance( $instance );
